f5f1bdb0bf9dcef1a00c8605dba03e8a64c5be90
nallamalli476 <69727828+nallamalli476@users.noreply.github.com>
2021-06-08 17:20:13 +0530

    Add files via upload

---
+++ src/main/java/net/javaguides/springboot/City.java
@@ -0,0 +1,32 @@
+package com.entrego.pod.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class City {
+
+	@JsonProperty(value = "cityId")
+	private int cityId;
+	@JsonProperty(value = "cityName")
+	private String cityName;
+
+	public int getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+}
